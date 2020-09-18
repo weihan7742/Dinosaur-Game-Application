726b0587be980dbaf0bcbec4c9b5da5cf1378ac6
Ng Wei Han <weihan774237@gmail.com>
2020-09-18 12:04:39 +0800

    Grass has higher probability to spawn when near grass

--- src/game/Grass.java
+++ src/game/Grass.java
@@ -1,12 +1,14 @@
 package game;
 
 import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
 
 public class Grass extends Ground {
+    private int turn;
+    private Probability probability = new Probability();
 
     public Grass() {
         super('^');
     }
-
 
 }

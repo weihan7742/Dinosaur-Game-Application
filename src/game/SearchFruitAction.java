bd2f172e2d9c1f5e5618b3dd4e92d14820c361fd
Kar Kei Ong <kong0016@student.monash.edu>
2020-09-18 12:34:14 +0800

    Pick Fruit from Tree

---
+++ src/game/SearchFruitAction.java
@@ -0,0 +1,38 @@
+package game;
+
+import edu.monash.fit2099.engine.*;
+
+public class SearchFruitAction extends Action {
+
+    private Ground tree;
+    private Location location;
+    private Item fruit =  new Fruit();
+    private Probability probability = new Probability();
+
+    public SearchFruitAction(Ground tree, Location location) {
+        tree = this.tree;
+        location = this.location;
+    }
+
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        char treeChar = location.getDisplayChar();
+
+        if (treeChar == '+' || treeChar == 't' || treeChar == 'T') {
+            if (probability.calculateProbability(40)) {
+                actor.addItemToInventory(fruit);
+            } else {
+                System.out.println("You search the tree for fruits, but you can't find any ripe ones.");
+            }
+        }
+        return menuDescription(actor);
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + "picked a Fruit from the Tree";
+    }
+
+
+}

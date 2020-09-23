cadfd9b200bf1e98725583c66ec9a32ebe762745
Kar Kei Ong <kong0016@student.monash.edu>
2020-09-23 22:54:15 +0800

    Updated food points

--- src/game/EatFoodAction.java
+++ src/game/EatFoodAction.java
@@ -2,13 +2,11 @@ package game;
 
 import edu.monash.fit2099.engine.*;
 
-import java.util.HashMap;
-
 public class EatFoodAction extends Action {
     private Ground grass;
     private Item food;
     private Dinosaur dino;
-    private HashMap<Character, Integer> foodPoints = new HashMap<Character, Integer>();
+    private FoodPoints foodpoints = new FoodPoints();
 
     public EatFoodAction(Actor dino, Item food) {
         this.dino = (Dinosaur) dino;
@@ -20,31 +18,24 @@ public class EatFoodAction extends Action {
         this.grass = grass;
     }
 
-    private void createFoodPoints() {
-        foodPoints.put('o', 30);
-        foodPoints.put('h', 20);
-        foodPoints.put('^', 5);
-    }
 
 
     @Override
     public String execute(Actor actor, GameMap map) {
-        createFoodPoints();
         if (actor.isConscious()) {
             if (map.locationOf(actor).getGround().getDisplayChar() == '^') {
                 map.locationOf(actor).setGround(new Dirt());
             }
         }
         if (food != null) {
-            dino.fed(actor, foodPoints.get(food.getDisplayChar()));
+            dino.fed(actor, foodpoints.getFoodPoints().get(food.getDisplayChar()));
             return menuDescription(actor) + food;
         } else if (grass != null) {
-            dino.fed(actor, foodPoints.get(grass.getDisplayChar()));
+            dino.fed(actor, foodpoints.getFoodPoints().get(grass.getDisplayChar()));
             return menuDescription(actor) + "grass";
         }
         return null;
     }
-
 
     @Override
     public String menuDescription (Actor actor){
---
+++ src/game/FoodPoints.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.util.HashMap;
+
+public class FoodPoints {
+
+    private HashMap<Character, Integer> foodPoints = new HashMap<Character, Integer>();
+
+    private void createFoodPoints() {
+        foodPoints.put('o', 30);
+        foodPoints.put('h', 20);
+        foodPoints.put('^', 5);
+    }
+
+    public HashMap<Character, Integer> getFoodPoints() {
+        return new HashMap<>(foodPoints);
+    }
+}

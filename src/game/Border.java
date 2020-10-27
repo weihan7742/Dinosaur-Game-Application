0b81f18774856b52ce2dfce142834e15e29c57c4
Ng Wei Han <weihan774237@gmail.com>
2020-10-27 14:12:58 +0800

    Added full second map feature. Player can move across between maps now.

--- src/game/Border.java
+++ src/game/Border.java
@@ -6,17 +6,26 @@ public class Border extends Ground {
 
     private Location newLocation;
     private String newDirection;
+    private Boolean displayOnMenuOrNot;
 
-    public Border(Location location, String direction){
+    public Border(Location location, String direction, Boolean displayOrNot){
         super('-');
         this.newLocation = location;
         this.newDirection = direction;
+        this.displayOnMenuOrNot = displayOrNot;
     }
 
     @Override
     public Actions allowableActions(Actor actor, Location location, String direction){
         Actions actions = super.allowableActions(actor,location,direction);
-        actions.add(new MoveActorAction(newLocation,newDirection));
+        if(displayOnMenuOrNot){
+            actions.add(new MoveActorAction(newLocation,newDirection));
+        }
         return actions;
+    }
+
+    @Override
+    public boolean canActorEnter(Actor actor) {
+        return false;
     }
 }

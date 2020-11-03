ae12606483923ec964d0b7597279680ecbf4b5c8
Ng Wei Han <weihan774237@gmail.com>
2020-11-03 16:06:04 +0800

    Documentations added.

--- src/game/EndGameAction.java
+++ src/game/EndGameAction.java
@@ -4,6 +4,9 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * Action to allow player to quit game at any point of time.
+ */
 public class EndGameAction extends Action {
 
     @Override
--- src/game/ModeDisplay.java
+++ src/game/ModeDisplay.java
@@ -5,11 +5,20 @@ import edu.monash.fit2099.engine.Display;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Class which allows user to input number and choose game modes.
+ */
 public class ModeDisplay {
 
     private Display display = new Display();
     private ArrayList<String> modes = new ArrayList<>();
 
+    /**
+     * Allow user to choose game modes.
+     *
+     * @param player Player playing
+     * @return integer representing mode chosen
+     */
     public int chooseMode(Player player){
         displayDivider();
         addMode("Challenge");
@@ -24,6 +33,13 @@ public class ModeDisplay {
         return input;
     }
 
+    /**
+     * Allow user to input integer
+     *
+     * @param condition String to be displayed to prompt user
+     * @param chooseMode true if user wants to choose mode, false otherwise.
+     * @return integer
+     */
     public int inputNumber(String condition, boolean chooseMode){
         Scanner scanner = new Scanner(System.in);
         boolean valid = false;
@@ -33,6 +49,7 @@ public class ModeDisplay {
                 display.println(condition);
                 input = scanner.nextInt();
 
+                // Ensure user does not input out or range when choosing mode.
                 if(chooseMode){
                     if(input <= 0 || input > modes.size()){
                         throw new Exception("Invalid input");
@@ -50,10 +67,18 @@ public class ModeDisplay {
         return input;
     }
 
+    /**
+     * Allow different game modes to be added.
+     *
+     * @param newMode String representing of new mode.
+     */
     public void addMode(String newMode){
         modes.add(newMode);
     }
 
+    /**
+     * Display divider represented by '-'
+     */
     public void displayDivider(){
         display.println("----------------------");
     }
